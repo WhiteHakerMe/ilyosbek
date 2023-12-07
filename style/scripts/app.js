6343f555e50e0343f1b9a70a138f301fc0357628
haker <whitehaker010@gmail.com>
2023-12-08 01:36:57 +0500

    main

---
+++ style/scripts/app.js
@@ -0,0 +1,12 @@
+let nuqtaElement = document.getElementById('nuqta');
+let orage = document.createElement('span');
+
+orage.textContent = '.';
+orage.style.color = '#FF6E30';
+orage.style.fontSize = '50px'
+orage.style.fontWeight = '500'
+orage.style.lineHeight = '140%'
+nuqtaElement.appendChild(orage)
+
+let button = document.getElementById('button');
+
