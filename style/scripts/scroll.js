14d2e247b6672b28c94d8deadf91a32b95d4f1f9
haker <140898334+WhiteHakerMe@users.noreply.github.com>
2023-12-11 14:06:40 +0500

    main

---
+++ style/scripts/scroll.js
@@ -0,0 +1,31 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('.icon-png', {
+    scale:2,
+    z:1,
+    ease:"ease",
+})
+
+
+const timeline = gsap.timeline();
+
+timeline.from('.banner-catalog_title', {
+    y:150,
+    skewY:5,
+    duration:1.2
+}).from('.banner-catalog_text', {
+    letterSpacing:-10,
+    opacity:0,
+    duration:1.6
+}).from('.banner-form', {
+    x:-100,
+    skewY:-4,
+    opacity:0,
+    duration:2
+}).from('.header-nav', {
+    letterSpacing:10,
+    opacity:0,
+    y:-120,
+    skewY:-13,
+    duration:1
+});
